2264e44fc2894a4c9d5bf486470c6a38afc7ca88
wachirayeamnim42 <6306021412059@fitm.kmutnb.ac.th>
2021-03-11 14:20:15 +0700

    create

--- showdata.php
+++ showdata.php
@@ -1,10 +1,16 @@
 <?php
-    define("hostname","localhost");
-    define("user","user");
-    define("password","a2fSVa1VMuQeuFeV");
-    define("dbname","bookstore");
+define("hostname", "localhost");
+define("user", "user");
+define("password", "a2fSVa1VMuQeuFeV");
+define("dbname", "bookstore");
 ?>
 
 <?php
-    $mysql = new mysqli(hostname, )
+$mysql = new mysqli(hostname, user, password, dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
